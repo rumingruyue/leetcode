7a12f5da5ddb025e7262cec7b8067ddbbf99195d
刘汪洋 <liuwangyang@corp.netease.com>
2018-04-17 12:54:05 +0800

    提交easy类

--- src/com/chujianyun/leetcode/Easy.java
+++ src/com/chujianyun/leetcode/Easy.java
@@ -1,5 +1,18 @@
 package com.chujianyun.leetcode;
 
-public class TwoSum {
-    
+public class Easy {
+
+
+    /**
+     * 整形数组，找出和为某数的两个数字
+     * @param nums  整型数组
+     * @param target 目标数字
+     * @return 满足和为target的两个整数
+     */
+    public int[] twoSum(int[] nums, int target) {
+
+        return null;
+    }
+
+
 }
